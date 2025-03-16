abb332f7a826d80eda1a95bd25d1363f7aa6bd70
DanielRegiani <ddaniel_23d@hotmail.com>
2025-03-16 14:13:32 +0000

    inicio projeto supertrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 
-int main() {
+int main(){
     printf("Desafio Super Trunfo\n");
     printf("Novo Commit\n");
+
+    printf("Carta 1: \n");
+
+    char estado = 'A';
+    char codigo[04];
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+    int numeropontosturisticos;
+
+    printf("Estado: \n");
+    scanf("%c", &estado);
+    printf("Código: \n");
+    scanf("%s", &codigo);
+    printf("Nome da Cidade: \n");
+    scanf("%s", &nome);
+    printf("População: \n");
+    scanf("%d", &populacao);
+    printf("Área: \n");
+    scanf("%f", &area);
+    printf("PIB: \n");
+    scanf("%f", &pib);
+    printf("Número de Pontos Turísticos: \n");
+    scanf("%d", &numeropontosturisticos);
+
+    printf("Estado: %c \n", estado);
+    printf("Código: %s \n", codigo);
+    printf("Nome da Cidade: %s \n", nome);
+    printf("População: %d \n", populacao);
+    printf("Área: %f \n", area);
+    printf("PIB: %f \n", pib);
+    printf("Número de pontos Turísticos: %d \n", numeropontosturisticos);
+
 
     return 0;
 }
